a3d5e6a984aae2324fd54d03d1501c292c442ec1
xiaofeng <xiaofeng@xf-network>
2020-07-15 11:01:12 +0800

    增加返回结果封装和日期处理工具类

---
+++ src/main/java/com/xiaofeng/utils/DateUtils.java
@@ -0,0 +1,27 @@
+package com.xiaofeng.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * @ClassName:  DateUtils   
+ * @Description: 日期处理工具类
+ * @date:   2020年7月15日 上午10:16:02
+ */
+public class DateUtils {
+
+	/**
+	 * 
+	 * @Title: getNowDateToString   
+	 * @Description: 当前时间转字符串
+	 * @param: @return      
+	 * @return: String      
+	 * @throws
+	 */
+	public static String getNowDateToString() {
+		Date nowDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sdf.format(nowDate);
+	}
+}
---
+++ src/main/java/com/xiaofeng/utils/Result.java
@@ -0,0 +1,29 @@
+package com.xiaofeng.utils;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Data;
+
+/**
+ * 
+ * @ClassName:  Result   
+ * @Description: 返回对象
+ * @date:   2020年7月15日 上午10:20:09
+ */
+@Data
+public class Result implements Serializable{
+
+	private int code;
+	private String content;
+	private Map<String,Object> info = new HashMap<String, Object>();
+	
+	public static Result of(String content) {
+		Result r = new Result();
+		r.code = 0;
+		r.content = content;
+		return r;
+		
+	}
+}
