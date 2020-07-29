b2afbfb3b766fa9feb73afa217346d08b114d131
小峰 <160499844@qq.com>
2020-07-29 17:55:54 +0800

    接收消息打印日志

--- src/main/java/com/xiaofeng/queue/MessageReceiver.java
+++ src/main/java/com/xiaofeng/queue/MessageReceiver.java
@@ -2,10 +2,11 @@ package com.xiaofeng.queue;
 
 import com.xiaofeng.netty.server.DynMessage;
 import com.xiaofeng.utils.MessageVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 @RabbitListener(queues = "hello")
 public class MessageReceiver {
@@ -17,7 +18,7 @@ public class MessageReceiver {
     @RabbitHandler
     public void process(MessageVo messageVo) {
         String jsonString = com.xiaofeng.utils.string.StringUtils.toJson(messageVo);
-        System.out.println("消息队列收到消息  : " + jsonString);
+        log.info("消息队列收到消息  : " + jsonString);
         DynMessage.broadcast(messageVo.getGroupId(), jsonString);
     }
 
