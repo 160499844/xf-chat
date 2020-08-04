29619f806e12ec1b46b12d6e9cc01b5e2d13f862
小峰 <160499844@qq.com>
2020-08-04 09:47:30 +0800

    优化常量类

--- src/main/java/com/XfChatApplication.java
+++ src/main/java/com/XfChatApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import java.net.InetSocketAddress;
 
+import com.xiaofeng.global.UtilConstants;
 import com.xiaofeng.queue.MessageRunnable01;
 import com.xiaofeng.queue.MessageRunnable02;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +14,7 @@ import com.xiaofeng.netty.server.NettyServer;
 @EnableMongoPlus
 @SpringBootApplication
 public class XfChatApplication {
-	
+
 
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();    //获取开始时间
@@ -25,7 +26,7 @@ public class XfChatApplication {
         new Thread(new MessageRunnable02()).start();
         long endTime = System.currentTimeMillis();    //获取结束时间
         System.out.println("启动消耗时间:" + ((endTime - startTime)/1000) + "秒");    //输出程序运行时间
-        nettyServer.start(new InetSocketAddress("0.0.0.0", 8900));
+        nettyServer.start(new InetSocketAddress(UtilConstants.NettyConfigConstants.HOST_NAME, UtilConstants.NettyConfigConstants.HOST_PORT));
 	}
 	
 	
